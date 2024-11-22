b24b7158ed0a361bbbe14405919d1a9f62b774e7
DevItCLP <it@cateringclp.com>
2024-11-22 12:53:11 -0500

    upload files

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,18 @@ const nextConfig = {
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL, // Define NEXTAUTH_URL
   },
+  async headers() {
+    return [
+      {
+        source: "/api/auth/(.*)", // Aplica a todas las rutas de autenticación de NextAuth
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Permite solicitudes desde tu dominio Amplify
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
